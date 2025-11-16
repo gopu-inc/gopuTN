8a069a3fdf8ceab82cdafddd8cd573c857e03045
Mauricio-100 <ceoseshell@gmail.com>
2025-11-16 13:31:13 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
 setup(
     name="gotn",
     version="0.2.1",
@@ -9,4 +12,6 @@ setup(
             "gotn=gotn.cli:main",
         ],
     },
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 )
