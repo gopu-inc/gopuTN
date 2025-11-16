ce666f54e2e5d510823a8dc4f0a749f2951c1f4c
Mauricio-100 <ceoseshell@gmail.com>
2025-11-16 14:49:49 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="gotn",
-    version="0.2.2",
+    version="0.2.3",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
