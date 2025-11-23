30c8152cc74d0d8a216e11a93a4e94127d828d2e
Mauricio-100 <ceoseshell@gmail.com>
2025-11-23 01:39:39 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="gotn",
-    version="0.2.19",
+    version="0.3.0",
     packages=find_packages(),
      install_requires=[
         "requests",
